74e5692e0fdd0c7b2c8a6075d3fa94a411209758
Kenichi Nakamura <nkkn1008@gmail.com>
2013-01-04 15:57:21 +0900

    libファイルのmakeファイル作成練習

---
+++ LIB/Sample.h
@@ -0,0 +1,6 @@
+#ifndef SAMPLE_H
+#define SAMPLE_H
+
+int hoge();
+
+#endif
--- TestCode/mytest.cpp
+++ TestCode/mytest.cpp
@@ -1,8 +1,16 @@
 #include "/Users/centvilla29/Documents/src/CPP/gtest-1.6.0/include/gtest/gtest.h"
+#include "../LIB/Sample.h"
 
 TEST(firstTest, abs)
 {
-  EXPECT_EQ(1, abs( -1 ));
-  EXPECT_EQ(1, abs( 1 ));
+	EXPECT_EQ(1, abs( -1 ));
+	EXPECT_EQ(1, abs( 1 ));
 }
 
+TEST(firstTest, linklib)
+{
+	int ret = -1;
+
+	ret = hoge();
+	EXPECT_EQ(0, ret);	   
+}
